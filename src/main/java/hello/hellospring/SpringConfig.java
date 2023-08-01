e8543c4206511e21ee2d6ec5a53f014ffcbf610a
one031411 <one031411@naver.com>
2023-08-01 17:10:12 +0900

    jpa 테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,24 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -26,6 +29,6 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 
-        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
